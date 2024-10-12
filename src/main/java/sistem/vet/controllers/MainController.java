eb560bdb3add91d8f6a712d8c3810e1e36ca80a6
GustavoOlSantos <santos.gustavo.of@gmail.com>
2024-10-11 21:16:57 -0300

    Implementação do toggle no SideMenu

--- src/main/java/sistem/vet/controllers/MainController.java
+++ src/main/java/sistem/vet/controllers/MainController.java
@@ -12,19 +12,27 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 public class MainController implements Initializable {
     
     @FXML
-    public VBox contentBox; // This should be the VBox where you want to load the content
+    public VBox contentBox; //=> Divisão que deve receber os conteúdos atualizáveis
     @FXML
-    public HBox PI;
+    public HBox PI;			//=> Pagina Inicial
     @FXML
-    public HBox LC;
+    public HBox LC;			//=> Lista de Clientes
     @FXML
-    public HBox HC;
+    public HBox HC;			//=> Histórico de Clientes
+    @FXML
+    public HBox MenuBox;	//=> Container do Menu
+    @FXML
+    public VBox leftMenu; //=> Menu Lateral
+    private boolean isDisplay = true; //=> Estado do menu
     
     @FXML
     public void ClickHome() {
@@ -85,6 +93,27 @@ public class MainController implements Initializable {
 	        }
         }
     }
+    
+    @FXML
+    public void ClickMenu() {
+        // Toggle visibility
+        if (isDisplay) {
+            leftMenu.setPrefWidth(65); 
+            leftMenu.setMinWidth(65);
+            leftMenu.setMaxWidth(65);      
+            
+            toggleMenu();
+        } 
+        else {
+            leftMenu.setPrefWidth(350); // Show the menu by setting it back to its width
+            leftMenu.setMinWidth(250);
+            leftMenu.setMaxWidth(450);
+            
+            toggleMenu();
+        }
+        
+        isDisplay = !isDisplay; // Update the menu state
+    }
 
     private void loadContent(String fxmlFile) {
         try {
@@ -104,6 +133,40 @@ public class MainController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    	 loadContent("home.fxml");
+    	loadContent("home.fxml");
+    	toggleMenu();
     }    
+    
+    private void toggleMenu() {
+    	MenuBox.getChildren().forEach(node -> isLabel(node, isDisplay));
+        PI.getChildren().forEach(node -> isLabel(node, isDisplay));
+        LC.getChildren().forEach(node -> isLabel(node, isDisplay));
+        HC.getChildren().forEach(node -> isLabel(node, isDisplay));
+        
+        Rectangle clip = new Rectangle(leftMenu.getPrefWidth(), leftMenu.getHeight());
+        leftMenu.setClip(clip);
+
+        //=> Ajuste dinâmico do tamanho do clip
+        leftMenu.widthProperty().addListener((obs, oldVal, newVal) -> clip.setWidth(newVal.doubleValue()));
+        leftMenu.heightProperty().addListener((obs, oldVal, newVal) -> clip.setHeight(newVal.doubleValue()));
+    }
+
+   private void isLabel(Node node, boolean isDisplay) {
+    	
+    	if (node instanceof Label && isDisplay) {
+            node.setVisible(false); 
+            node.setOpacity(0);
+            node.prefWidth(0);
+        }
+    	
+    	else if (node instanceof Label && !isDisplay) {
+            node.setVisible(true);  
+            node.setOpacity(1);
+            node.prefWidth(200);
+        }
+    	
+    	else {
+    		return;
+    	}
+    }
 }
